91f36ad8a0ae0e87d6d640b4350a3df111db6393
ZsoltNagy91 <zsolt.j.nagy@gmail.com>
2026-09-22 21:43:06 +0200

    Test posting data

--- obj/Debug/net10.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/TodoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c35d7dea2dc41a86716b8700ae3ed99d1d6654e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8b6974f8f803e12f07ed30fbc88dd50801631eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
